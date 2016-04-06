455afe329770eb875e0a36f92dc2cb2f92f00d6b
SamElvi <kemosabe2013@163.com>
2016-04-06 17:40:42 +0800

    this is the comme

---
+++ Application/Home/Model/ChangecodeModel.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sammy
+ * Date: 16/4/6
+ * Time: 下午5:39
+ */
+
+namespace Home\Model;
+use Think\Model;
+
+class ChangecodeModel extends BaseModel{
+
+
+
+}
