e61025b0f23fec1a9fb3c48110b8aeae9b5db2d9
SamElvi <kemosabe2013@163.com>
2016-04-09 21:13:39 +0800

    this is the comme

---
+++ Application/Home/Controller/MessageController.class.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sammy
+ * Date: 16/4/9
+ * Time: 下午4:32
+ */
+namespace Home\Controller;
+use Think\Controller;
+
+class MessageController extends BaseController{
+
+    public function booksmail(){
+        $mail = I('post.mail');
+        $touserid = I('post.touserid');
+        if(!empty($mail) && !empty($touserid) && !empty($this->_userInfo['userid'])){
+            $db_mail = D('mail');
+            $sent = $db_mail->insertMailByOption(array(
+                'fromid'=>$this->_userInfo['userid'],
+                'toid'=>$touserid,
+                'content'=>$mail,
+                'fromname'=>$this->_userInfo['username']
+            ));
+            if($sent){
+                $error['name'] = '邮件发送啦';
+                $error['code'] = 1;
+            }else{
+                $error['name'] = '额~邮件太多了,交给了EMS,你懂的...';
+                $error['code'] = 2;
+            }
+        }else{
+            $error['name'] = '邮件信息不完整,不能投递哦';
+            $error['code'] = 0;
+        }
+        $this->ajaxReturn($error,"json");
+    }
+
+
+}
