5488022dd21ff00df4511f8fc2e2a5c3f3974a14
SamElvi <kemosabe2013@163.com>
2016-04-10 10:47:49 +0800

    this is the comme

--- Application/Home/Model/MailModel.class.php
+++ Application/Home/Model/MailModel.class.php
@@ -15,11 +15,11 @@ class MailModel extends BaseModel{
         return $md_mail->add($option);
     }
 
-    public function updateMailById($mailid,$status=1){
+    public function updateMailById($mailid,$data){
         if($mailid == '')
             return false;
         $md_mail = D('mail');
-        return $md_mail->where('id = '.$mailid)->save(array('status'=>$status));
+        return $md_mail->where('id = '.$mailid)->save($data);
     }
 
     public function getMailsByFromStatus($fromid){
@@ -27,16 +27,40 @@ class MailModel extends BaseModel{
             return null;
         $md_mail = D('mail');
         $option['fromid'] = $fromid;
-        $option['status'] = array('neq',2);;
+        $option['del'] = 0;
         return $md_mail->where($option)->select();
     }
 
-    public function getMailsByToStatus($toid,$status=0){
+    public function getMailsByToStatus($toid){
         if($toid =='')
             return null;
         $md_mail = D('mail');
         $option['toid'] = $toid;
-        $option['status'] = array('neq',3);;
+        $option['status'] = array('lt','2');
         return $md_mail->where($option)->select();
+    }
+
+    public function getMailNumByIdAndStatus($uid,$status=0){
+        if(empty($uid))
+            return null;
+        $md_mail = D('mail');
+        $option['toid'] = $uid;
+        $option['status'] = $status;
+        return $md_mail->where($option)->count();
+    }
+
+    public function watchMailStatusByToid($toid){
+        if(empty($toid))
+            return null;
+        $md_model = D('mail');
+        $option['toid'] = $toid;
+        $option['status'] = 0;
+        $data['status']=1;
+        $list = $md_model->where($option)->save($data);
+//        $num = 0;
+//        foreach($list as $one){
+//           $num+= $md_model->where('id ='.$one['id'])->save($data);
+//        }
+        return $list;
     }
 }
